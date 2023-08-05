a3b6280ff2144b414dd6e824a78a18a3dd2855f9
drhenrich <drhenrich@outlook.com>
2023-08-05 14:09:16 +0200

    Update voicechat.py

--- voicechat.py
+++ voicechat.py
@@ -6,36 +6,35 @@ import openai
 openai.api_key='sk-xxxx'
 # Initialize the recognizer
 r = sr.Recognizer()
-
-# Use the Raspberry Pi's default microphone as the audio source
-with sr.Microphone() as source:
-    print("Listening...")
-    audio = r.listen(source)
-
-try:
-    # Recognize speech using Google Speech Recognition
-    text = r.recognize_google(audio)
-    print("You said:", text)
-
-    # Convert text to speech using gTTS
-    tts = gTTS(text=text, lang='en')
-    tts.save("output.mp3")
-
-    # Play the generated speech output using mpg321
-    os.system("mpg321 output.mp3")
+while True:
+    # Use the Raspberry Pi's default microphone as the audio source
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
     
-    # Send input to OpenAI API
-    response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}])
-    response_text = response.choices[0].message.content
-    print(response_text)
+    try:
+        # Recognize speech using Google Speech Recognition
+        text = r.recognize_google(audio)
+        print("You said:", text)
+    
         # Convert text to speech using gTTS
-    tts = gTTS(text=response_text, lang='en')
-    tts.save("output.mp3")
-
-    # Play the generated speech output using mpg321
-    os.system("mpg321 output.mp3")
+        tts = gTTS(text=text, lang='en')
+        tts.save("output.mp3")
     
-
+        # Play the generated speech output using mpg321
+        os.system("mpg321 output.mp3")
+        
+        # Send input to OpenAI API
+        response = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=[{"role": "user", "content": f"{text}"}])
+        response_text = response.choices[0].message.content
+        print(response_text)
+            # Convert text to speech using gTTS
+        tts = gTTS(text=response_text, lang='en')
+        tts.save("output.mp3")
+    
+        # Play the generated speech output using mpg321
+        os.system("mpg321 output.mp3")
+    
 except sr.UnknownValueError:
     print("Sorry, I could not understand what you said.")
 except sr.RequestError as e:
